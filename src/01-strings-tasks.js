1f359f3f7c81101669c8e99bb03432b2896463a6
vladislav_kovaliov <vkov2001@gmai.com>
2021-12-17 22:34:28 +0300

    isString complete

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -250,8 +250,9 @@
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+ function isString( value ) {
+  //throw new Error('Not implemented');
+  return typeof value === "string" ? true : value instanceof String;
 }
 
 /**
