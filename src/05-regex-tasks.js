aca5424126f18520ec952d18be423688aa0f80f5
vladislav_kovaliov <vkov2001@gmai.com>
2021-12-17 23:00:36 +0300

    regex complete

--- src/05-regex-tasks.js
+++ src/05-regex-tasks.js
@@ -30,8 +30,8 @@
  *
  * @return {RegExp}
  */
-function getRegexForGuid() {
-  throw new Error('Not implemented');
+ function getRegexForGuid() {
+  return /\{[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}\}/
 }
 
 /**
@@ -52,7 +52,7 @@ function getRegexForGuid() {
  *
  */
 function getRegexForPitSpot() {
-  throw new Error('Not implemented');
+  return /s.+|.+i.+/
 }
 
 /**
@@ -75,8 +75,8 @@ function getRegexForPitSpot() {
  *   'PASSW0RD'.match(validator)  => false
  *   'Pa55'.match(validator) => false
  */
-function getPasswordValidator(/* minLength */) {
-  throw new Error('Not implemented');
+function getPasswordValidator( minLength ) {
+  return new RegExp("^(?=.*[a-z]+)(?=.*[A-Z]+)(?=.*[0-9]+)[a-zA-Z0-9]{" + minLength + ",}$")
 }
 
 module.exports = {
